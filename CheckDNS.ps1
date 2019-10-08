01efa21414fef80cbc68a6e597302d46cdd2c18d
AlejandroMoraG <43340016+AlejandroMoraG@users.noreply.github.com>
2019-10-08 14:16:45 +0200

    Update CheckDNS.ps1

--- CheckDNS.ps1
+++ CheckDNS.ps1
@@ -11,29 +11,29 @@ Write-Host "-------------------" -ForegroundColor Green
 
 $count=0
 $date=(get-date)
-echo "-----------------DNS Report-------------------" >> C:\Users\A743903\Desktop\PWSH\informe.txt
-echo $date >> C:\Users\A743903\Desktop\PWSH\informe.txt
-echo "----------------------------------------------" >> C:\Users\A743903\Desktop\PWSH\informe.txt
+echo "-----------------DNS Report-------------------" >> C:\Users\user\Desktop\PWSH\informe.txt
+echo $date >> C:\Users\user\Desktop\PWSH\informe.txt
+echo "----------------------------------------------" >> C:\Users\user\Desktop\PWSH\informe.txt
 
 #DNS and Ping report
-foreach($name in Get-Content C:\Users\A743903\Desktop\PWSH\lista.txt) {
+foreach($name in Get-Content C:\Users\user\Desktop\PWSH\lista.txt) {
     $dns=$name+".yoigo.com"
 	$respuesta=(Resolve-DnsName $dns)
 	$ping=((Test-NetConnection $dns).PingSucceeded)
 	if($respuesta -ne $null){
 		$salida=$dns+" --> IP:"+$respuesta.IPAddress+" (Ping:"+$ping+")"
-		echo "$salida" >> C:\Users\A743903\Desktop\PWSH\informe.txt
+		echo "$salida" >> C:\Users\user\Desktop\PWSH\informe.txt
 		}
 	else{
 		$salidaf=$dns+" no encontrado (Ping:"+$ping+")"
-		echo "$salidaf" >> C:\Users\A743903\Desktop\PWSH\informe.txt
+		echo "$salidaf" >> C:\Users\user\Desktop\PWSH\informe.txt
 		$count++
 	}
 }
 if($count -gt 0){ 
 	$badnames=$count.ToString()+" DNS names not found"
-	echo "----------------------------------------------" >> C:\Users\A743903\Desktop\PWSH\informe.txt
-	echo "$badnames" >> C:\Users\A743903\Desktop\PWSH\informe.txt
+	echo "----------------------------------------------" >> C:\Users\user\Desktop\PWSH\informe.txt
+	echo "$badnames" >> C:\Users\user\Desktop\PWSH\informe.txt
 }
 Write-Host "-------------------" -ForegroundColor Red
 Write-Host "Names not found: " $counter -ForegroundColor Red
